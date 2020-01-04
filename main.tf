7f7bc68f95c36a44bfbb94b8ad3974cb476a3b0c
STEPHEN STRUTT <steve_strutt@uk.ibm.com>
2020-01-04 18:13:14 +0000

    new

--- main.tf
+++ main.tf
@@ -44,15 +44,20 @@ output "Output_vars" {
   value = "${data.ibm_schematics_output.test.output_values}"
 }
 
+output "RemoteHostName" {
+  value = "${data.ibm_schematics_output.test.output_values.}"
+}
+
 # output "StateStoreValues" {
 #   value = "${data.ibm_schematics_state.test.state_store}"
 # }
-
 
 # output "hostname" {
 #   value = "${data.ibm_schematics_state.test.state_store.ibm_compute_vm_instance.webapp1.hostname}"
 # }
 
-
 #     template_id  = "${data.ibm_schematics_workspace.test.template_id.0}"
-
+variable ssh_label {
+  description = "ssh label"
+  default     = "wcpclouduk1"
+}
